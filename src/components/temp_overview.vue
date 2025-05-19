f758a8cd82f4e16a3afefecdbd85045201f359a7
MischiMii <michelle_stoop@sluz.ch>
2025-05-19 13:33:45 +0200

    Created temp_overview.vue

--- src/components/temp_overview.vue
+++ src/components/temp_overview.vue
@@ -67,7 +67,7 @@
         </div>
 
         <!-- Start Shift -->
-        <div class="from-black-50 flex items-center justify-center rounded-xl bg-gradient-to-b to-purple-600 text-2xl font-bold">
+        <div class="from-black-50 flex items-start justify-start rounded-xl bg-gradient-to-b to-purple-600 p-4 text-3xl font-bold">
           START <br />
           SHIFT
         </div>
@@ -93,12 +93,11 @@
     <div class="flex h-[calc(100vh-2rem)] w-[250px] flex-col justify-between rounded-xl bg-gray-800 p-4 text-sm">
       <div>
         <h3 class="text-gray-400">Upcoming Session:</h3>
-        <p class="text-xl font-bold">MONDAY</p>
+        <p class="text-2xl font-bold">MONDAY</p>
         <p class="text-gray-400">24.03.25</p>
-      </div>
-      <div class="mt-4">
-        <p class="text-gray-400">Start:</p>
-        <p class="text-xl font-semibold">13:00 - 22:00</p>
+        <div class="mt-4">
+          <p class="text-4xl font-semibold">13:00 - 22:00</p>
+        </div>
       </div>
     </div>
   </div>
